870d619a0b2aff17318a66467f091a147af090d7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-05 13:54:27 +0200

    explicit CMake names

--- conanfile.py
+++ conanfile.py
@@ -111,6 +111,8 @@ class HarfbuzzConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "harfbuzz"
+        self.cpp_info.names["cmake_find_package_multi"] = "harfbuzz"
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include", "harfbuzz"))
         if self.settings.os == "Linux":
